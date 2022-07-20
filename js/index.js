cdc03f86d0a295f1773559f4143742ae21c95069
charlyfdz <carlos.afdzf@gmail.com>
2022-07-20 00:22:32 -0500

    editor de imagenes: rotate and flip image, save image, reset filters

--- js/index.js
+++ js/index.js
@@ -1,28 +1,50 @@
-const fileInput =  document.querySelector(".file-input"),
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
-filterSlider = document.querySelector(".slider input"),
-filterValue = document.querySelector(".slider .value"),
-previewImage = document.querySelector(".preview-img img"),
-chooseImgBtn = document.querySelector(".choose-img");
+const fileInput =  document.querySelector(".file-input")
 
-let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
+const filterOptions = document.querySelectorAll(".filter button"),
+      filterName = document.querySelector(".filter-info .name"),
+      filterSlider = document.querySelector(".slider input"),
+      filterValue = document.querySelector(".slider .value")
+
+const rotateOptions = document.querySelectorAll(".rotate button")
+
+const previewImage = document.querySelector(".preview-img img"),
+      previewImageContainer = document.querySelector(".preview-img")
+
+const chooseImgBtn = document.querySelector(".choose-img"),
+      resetFilterBtn = document.querySelector(".reset-filter"),
+      saveImageBtn = document.querySelector(".save-img")
+
+
+let brightness = 100, 
+    saturation = 100, 
+    inversion = 0, 
+    grayscale = 0;
+
+let rotate = 0,
+    flipHorizontal = 1;
+    flipVertical = 1;
 
 const applyFilters = ()=>{
+    previewImage.style.transform = `rotate(${rotate}deg) scale(${flipVertical}, ${flipHorizontal})`
     previewImage.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
 }
 
 const loadImage = ()=>{
+    
     let file = fileInput.files[0];
+
     if(!file) return
+
     previewImage.src = URL.createObjectURL(file);
     previewImage.addEventListener("load",()=>{
+        resetFilterBtn.click()
         document.querySelector(".container").classList.remove("disable")
     })
 }
 
 filterOptions.forEach(option=>{
     option.addEventListener("click",()=>{
+
         document.querySelector(".filter .active").classList.remove("active")
         option.classList.add("active")
         filterName.innerText = option.innerText
@@ -47,7 +69,24 @@ filterOptions.forEach(option=>{
     })
 })
 
+rotateOptions.forEach(option=>{
+
+    option.addEventListener("click",()=>{
+        if(option.id === "left"){
+            rotate -= 90;
+        }else if(option.id === "right"){
+            rotate += 90;
+        }else if(option.id === "vertical"){
+            flipVertical = flipVertical === 1 ? -1 : 1;
+        }else{
+            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
+        }
+        applyFilters()
+    })
+})
+
 const updateFilter = ()=>{
+
     filterValue.innerText = `${filterSlider.value}%`
     const selectedFilter = document.querySelector(".filter .active");
 
@@ -64,6 +103,43 @@ const updateFilter = ()=>{
     applyFilters()
 }
 
+const resetFilter = () =>{
+    brightness = 100
+    saturation = 100 
+    inversion = 0 
+    grayscale = 0
+
+    rotate = 0
+    flipHorizontal = 1
+    flipVertical = 1
+
+    filterOptions[0].click()
+
+    applyFilters()
+}
+
+const saveImage = ()=>{
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = previewImage.naturalWidth;
+    canvas.height = previewImage.naturalHeight;
+
+    ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
+    ctx.translate(canvas.width/2, canvas.height/2);
+    if(rotate !== 0){
+        ctx.rotate(rotate * Math.PI / 180);
+    }
+    ctx.scale(flipHorizontal, flipVertical);
+    ctx.drawImage(previewImage, -canvas.width /2,-canvas.height /2, canvas.width, canvas.height);
+
+    const link = document.createElement("a")
+    link.download = "edited.jpg"
+    link.href = canvas.toDataURL();
+    link.click();
+}
+
 fileInput.addEventListener("change", loadImage)
 filterSlider.addEventListener("input",updateFilter)
+resetFilterBtn.addEventListener("click",resetFilter)
+saveImageBtn.addEventListener("click",saveImage)
 chooseImgBtn.addEventListener("click",()=>fileInput.click())
